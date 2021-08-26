5f26a827099006a4fcce8c679175375824562126
zunayed-arp <zunayed.arp@gmail.com>
2021-08-26 14:55:33 +0600

    final-push

---
+++ script.js
@@ -0,0 +1,75 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if (theme==null) {
+	setTheme('light')
+} else {
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i = 0; themeDots.length > i; i++) {
+	themeDots[i].addEventListener('click', function () {
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+
+
+// for (const theme of themeDots) {
+// 	theme.addEventListener('click', function () {
+// 		let mode = this.dataset.mode;
+// 		console.log('option clicked', mode);
+// 		se
+// 	})
+// }
+
+// function setTheme(mode) {
+// 	// debugger;
+// 	if (mode == 'light') {
+// 		document.getElementById('theme-style').href = 'default.css';
+		
+// 	}
+// 	if (mode == 'blue') {
+// 		document.getElementById('theme-style').href = 'blue.css';
+// 		// document.style.backgroundColor = 'blue';
+// 	}
+// 	if (mode == 'green') {
+// 		document.getElementById('theme-style').href = 'green.css';
+// 		// document.style.backgroundColor = 'green';
+// 	}
+// 	if (mode == 'purple') {
+// 		document.getElementById('theme-style').href = 'purple.css';
+// 	// 	document.style.backgroundColor = 'purple';
+// 	// }
+
+// 	localStorage.setItem('theme', mode);
+// }
+
+
+
+function setTheme(mode) {
+	if (mode == 'light') {
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if (mode == 'blue') {
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+
+	if (mode == 'green') {
+		document.getElementById('theme-style').href = 'green.css'
+	}
+
+	if (mode == 'purple') {
+		document.getElementById('theme-style').href = 'purple.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
+
